82bf5b55baf070b684acddefe5a031a65841ab10
Niharikasahu0 <niharikaasahu001@gmail.com>
2025-07-30 14:24:11 +0530

    variables and data types

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello")
+// Variable=container used to store data values.
+// 1 var= avoid using in modern time
+// 2 let =Can be updated but not redeclared
+// 3 const= used for constants values.
+const accountId=12345
+let accountEmail="niharika@12345"
+var accountPassward="12345"
+var accountCity="bhopal"
+let accountState=
+
+// accountId=23   not allowed
+accountEmail="niharika@"
+accountPassward="11111"
+accountCity="jaipur"
+
+console.log(accountEmail);
+//table method
+console.table([accountCity,accountEmail,accountPassward,accountState])
+/*
+avoid use var 
+*/
+// "use strict" treat all js code as a newer version
+// Data types (numbers,string,BigInt,boolean,
+// null=(absence of any object value),undefined= A variable has been declared but no value has been assigned 
+// Symbol= unique obj)
+// Type of 
+console.log(typeof null);
+console.log(typeof undefined);
+
+
