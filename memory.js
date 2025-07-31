9937fe906dd8335a2153372666647b4547081b14
Niharikasahu0 <niharikaasahu001@gmail.com>
2025-07-31 12:53:11 +0530

    added memory

---
+++ memory.js
@@ -0,0 +1,42 @@
+//  Heap and stack
+let myname="Niharika"
+let lastname="sahu"
+lastname="thakur"
+console.log(lastname);
+
+let userone={
+    email: "@123",
+    upi:"@ybl"
+
+}
+let userTwo= userone
+userTwo.email='niharika@'
+console.log(userTwo.email);
+
+// STRINGS IN JS
+const name="niharika"
+const repoCount=10
+console.log(name.charArt(5));
+console.log(name.indexof(5));
+
+// console.log(name+ repoCount);(old way)
+// Backticks=modern way to work with strings 
+console.log(`hello ${name}and my repo count is ${repoCount}`);
+
+
+let msg = `This is line 1
+This is line 2
+This is line 3`;
+console.log(msg);
+
+let a = 5;
+let b = 10;
+console.log(`Sum of ${a} and ${b} is ${a + b}`); 
+
+
+
+
+
+
+
+
