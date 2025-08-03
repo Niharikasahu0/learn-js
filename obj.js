03c90a7eb81e2cdf6b0970613ccde7ce19a5d55e
Niharikasahu0 <niharikaasahu001@gmail.com>
2025-08-03 16:50:31 +0530

    Add funcition.js and obj.js

---
+++ obj.js
@@ -0,0 +1,46 @@
+// // Singleton
+// //object literals
+// const jsUsers={
+//     name:"Niharika",
+//     age: 22,
+//     location: "Bhopal",
+//     email: "n@123",
+// }
+// console.log(jsUsers.name);
+// console.log(jsUsers.age);
+// console.log(jsUsers.location);
+// console.log(jsUsers.email);
+
+// // Symbol in js
+// let id = Symbol("id");
+
+// let user = {
+//   name: "Mini",
+//   [id]: 101
+// };
+
+// console.log(user);       
+// console.log(user[id]);      
+
+// //freeze 
+// let user = {
+//   name: "Niharika",
+//   age: 22
+// };
+
+// Object.freeze(user);
+
+// user.age = 25;        
+// user.city = "Bhopal"; 
+// delete user.name;   
+//  console.log(user);   
+// //  Funcition is js
+// jsUser.greeting=function(){
+//     console.log("hello js user");
+    
+// }
+
+// PART 2
+const tinderUser = new Object();
+console.log(tinderUser);
+
